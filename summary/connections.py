de6b4bdb9f8e15ff9d435f77975ae723d6eca619
waiting <ttwait@gmail.com>
2015-09-11 09:52:12 +0800

    update connections

--- summary/connections.py
+++ summary/connections.py
@@ -9,11 +9,15 @@ def summary_locale():
 results_list = [
         summarize('stats_counts.do_engine.production.new_connections.*.ios.*', 'iOS New Connection'),
         summarize('stats_counts.do_engine.production.new_connections.*.android.*', 'Android New Connection'),
+        summarize('stats_counts.do_engine.production.*', 'en_US', extra_metric='.en_US.*.*'),
         summarize('stats_counts.do_engine.production.expired_connections.*.ios.*', 'iOS Expired Connection'),
         summarize('stats_counts.do_engine.production.expired_connections.*.android.*', 'Android Expired Connection'),
         summarize('stats_counts.do_engine.production.reconnected_connections.*.ios.*', 'iOS  ReConnection'),
         summarize('stats_counts.do_engine.production.reconnected_connections.*.android.*', 'Android  ReConnection'),
-        summarize('stats_counts.do_engine.production.*', 'en_US', extra_metric='.en_US.*.*'),
-        summarize('stats_counts.do_engine.production.*', 'All Locale', extra_metric='.*.*.*'),]
+        summarize('stats_counts.do_engine.production.*', 'All Locale', extra_metric='.*.*.*'),
+
+        summarize('stats.gauges.redis.production.processed_connections.en_US', 'en_US Processed Connections', extra_metric='.*.*'),
+        summarize('stats.gauges.redis.production.processed_connections.*.*.*', 'All Processed Connections'),
+        ]
 
 send_email(results_list)
