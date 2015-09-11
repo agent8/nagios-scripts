63c1b95460f2e75535e70da4bf6201f567645419
waiting <ttwait@gmail.com>
2015-09-11 10:44:09 +0800

    update

--- summary/connections.py
+++ summary/connections.py
@@ -16,7 +16,7 @@ results_list = [
         summarize('stats_counts.do_engine.production.reconnected_connections.*.android.*', 'Android  ReConnection'),
         summarize('stats_counts.do_engine.production.*', 'All Locale', extra_metric='.*.*.*'),
 
-        summarize('stats.gauges.redis.production.processed_connections.en_US', 'en_US Processed Connections', extra_metric='.*.*', sum_type='avg'),
+        summarize('stats.gauges.redis.production.processed_connections.en_US.*.*', 'en_US Processed Connections', sum_type='avg'),
         summarize('stats.gauges.redis.production.processed_connections.*.*.*', 'All Processed Connections', sum_type='avg'),
         ]
 
