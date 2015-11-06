eb11487ecfb407ea22f6bd2ef5b7c8e75ec0a26e
waiting <ttwait@gmail.com>
2015-11-06 12:36:27 +0800

    update

--- summary/returnpath.py
+++ summary/returnpath.py
@@ -6,7 +6,7 @@ from utils import *
 #stats_counts.returnpath_worker-python_worker_manager.production.i-92e03e32.returnpath.returnpath-gmail-classify.messages
 results_list = [
     summarize('stats_counts.worker.production.*.returnpath.*classify', 'Unique User', extra_metric='.finish_all_process'),
-    summarize('stats_counts.job_server.production.*.total_submitted_jobs.returnpath.*classify',
+    summarize('stats_counts.job_server.production.*.total_submitted_jobs.returnpath*',
               'Total Submitted Jobs'),
     summarize('stats_counts.worker.production.*.returnpath.*classify', 'Message', extra_metric='.messages'),
     summarize('stats_counts.worker.production.*.returnpath.*classify', 'Process Day', extra_metric='.process_day')
