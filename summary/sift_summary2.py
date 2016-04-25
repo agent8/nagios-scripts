e6565795481d687e23bc713bca4c074fb95dd43d
waiting <ttwait@gmail.com>
2016-04-25 14:14:47 +0800

    udpate

---
+++ summary/sift_summary2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding=utf8
+from utils import *
+
+
+#stats_counts.returnpath_worker-python_worker_manager.production.i-92e03e32.returnpath.returnpath-gmail-classify.messages
+results_list = [
+    summarize3('Open DB', 'Open DB'),
+    ]
+
+send_email(results_list)
