d4825c8bcf138e60c430fe8e605097493ebca3b1
waiting <ttwait@gmail.com>
2016-05-08 18:50:52 +0800

    fix update

--- summary/sift_summary.py
+++ summary/sift_summary.py
@@ -6,6 +6,7 @@ from utils import *
 #stats_counts.returnpath_worker-python_worker_manager.production.i-92e03e32.returnpath.returnpath-gmail-classify.messages
 results_list = [
     summarize3('Sift Worker', 'Sift Worker'),
+    summarize3('Sift Extract', 'Sift Extract'),
     summarize3('Open DB', 'Open DB'),
     summarize3('Open API', 'Open API'),
     summarize3('Flight Worker', 'Flight Worker'),
