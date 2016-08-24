489d022d764d9caf2acb75daff2856cdfe1fc6a2
waiting <ttwait@gmail.com>
2016-08-24 11:50:11 +0800

    fix email

--- summary/returnpath.py
+++ summary/returnpath.py
@@ -6,11 +6,11 @@ from utils import *
 #stats_counts.returnpath_worker-python_worker_manager.production.i-92e03e32.returnpath.returnpath-gmail-classify.messages
 results_list = [
     #summarize('stats.gauges.logs.worker.production.i-28ea5aa1.returnpath.returnpath*', 'Unique User', sum_type='latest', extra_metric='.unique_user'),
-    summarize3('ReturnPath'),
+    summarize3('ReturnPathClassify'),
     summarize3('ReturnPathToken'),
     summarize3('ReturnPathUserStatus'),
     summarize3('TotalReceipts'),
-    summarize3('Total Submitted ReturnPath Jobs'),
+    summarize3('Total Submitted ReturnPath Jobs', title='EDO Total Submitted Jobs'),
     #summarize('stats_counts.worker.production.i-28ea5aa1.returnpath.*classify', 'Unique User', extra_metric='.finish_all_process'),
     #summarize('stats_counts.job_server.production.*.total_submitted_jobs.returnpath*',
     #          'Total Submitted Jobs'),
